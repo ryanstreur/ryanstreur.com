4259d522cd5e0a6e7b34cd79aafba26d59aacf25
Ryan Streur <ryan.streur@rsginc.com>
2023-01-26 15:59:01 -0800

    add vCard

---
+++ README.md
@@ -0,0 +1,7 @@
+# ryanstreur.com
+
+## Relevant Documentation
+
+- vCard
+  - [RFC 6350: vCard](https://www.rfc-editor.org/rfc/rfc6350#page-9)
+  - [vCard: Wikipedia](https://en.wikipedia.org/wiki/VCard)
--- rsdc/lib/rsdc_web/endpoint.ex
+++ rsdc/lib/rsdc_web/endpoint.ex
@@ -20,7 +20,7 @@ defmodule RsdcWeb.Endpoint do
     at: "/",
     from: :rsdc,
     gzip: true,
-    only: ~w(assets fonts images favicon.ico robots.txt)
+    only: ~w(assets fonts images favicon.ico robots.txt misc)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
