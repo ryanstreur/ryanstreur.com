01c056b2b40c8c7c2ffe7152742fe53987220f38
Ryan Streur <ryan@ryanstreur.com>
2023-07-25 04:09:54 -0700

    update scaffolded client to make it a bit more my own

--- app/client/vite.config.ts
+++ app/client/vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import lightningcss from "vite-plugin-lightningcss";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    lightningcss({
+      browserslist: ">= 0.25%",
+    }),
+  ],
+});
