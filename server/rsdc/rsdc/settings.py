1dd1fc6b90ddddac0da4de913ea9631e9d62d0b5
Ryan Streur <ryan@ryanstreur.com>
2026-06-18 12:19:06 -0700

    add daphne and update to use asgi in dev

--- server/rsdc/rsdc/settings.py
+++ server/rsdc/rsdc/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+ASGI_APPLICATION = 'rsdc.asgi.application'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
