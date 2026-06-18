f1e52fb742b2cc87c6606abc08404a1ba1bd571d
Ryan Streur <ryan@ryanstreur.com>
2026-06-18 12:00:59 -0700

    add very basic homepage

--- server/rsdc/rsdc/urls.py
+++ server/rsdc/rsdc/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
+from django.shortcuts import render
 from django.urls import path
 
+def home(request):
+    return render(request, 'home.html')
+
 urlpatterns = [
+    path("", home),
     path('admin/', admin.site.urls),
 ]
